49d05c1e1826e81b3f408005563c3ceec8103374
Frank <frank.schneider@tutanota.com>
2023-03-30 17:38:13 +0200

    wip: fix price getter; add icons

--- src/views/cryptoCurrencies.vue
+++ src/views/cryptoCurrencies.vue
@@ -5,7 +5,7 @@
         <v-main>
             <v-navigation-drawer width="400">
                 <v-list>
-                    <v-list-item title="Controls" class="small-caps bg-grey"></v-list-item>
+                    <v-list-item class="small-caps text-h6 bg-grey">Controls</v-list-item>
                 </v-list>
 
                 <v-divider class="mb-4"></v-divider>
@@ -42,17 +42,15 @@
                             v-model="states.week"
                             label="Last week"
                             color="cyan"
-                            @click="getPrices(7, 'bitcoin'); getPrices(7, 'ethereum')"
-                        >
+                            >
                         </v-switch>
                     </v-col>
-
+                    
                     <v-col cols="6">
                         <v-switch
                             v-model="states.month"
                             label="Last month"
                             color="cyan"
-                            @click="getPrices(30, 'bitcoin'); getPrices(30, 'ethereum')"
                             >
                         </v-switch>
                     </v-col>
@@ -60,28 +58,42 @@
 
                 <span class="small-caps ml-4">... by date</span>
 
-                <v-form @submit.prevent="console.log(date)">
-                    <v-container>
+                <v-row class="ml-4">
+                    <v-col cols="6">
+                        <v-switch
+                            v-model="states.anytime"
+                            :label="states.anytime ? 'Hide' : 'Show'"
+                            color="blue-grey"
+                        >
+                        </v-switch>
+                    </v-col>
+                </v-row>
+
+                <v-form @submit.prevent>
+                    <v-container v-if="states.anytime">
                         <v-row>
                             <v-col
                                 cols="12"
                                 md="12"
                             >
-                                <v-text-field
-                                    v-model="date"
-                                    :counter="10"
-                                    label=""
-                                    required
-                                    placeholder="e.g. 14-02-2023"
-                                >
-                                </v-text-field>
+                            <input id="datetime" type="date" value="2023-03-14" />
+
+                            <v-btn
+                                inline
+                                class="small-caps text-white ml-4"
+                                color="grey"
+                                size="small"
+                                variant="flat"
+                                @click="getPrices('bitcoin', getDayDifference()); getPrices('ethereum', getDayDifference())"
+                                >Send
+                            </v-btn>
                             </v-col>
                         </v-row>
                     </v-container>
                 </v-form>
-            
-            <v-divider class="mb-8"></v-divider>
-            
+
+                <v-divider class="mb-8"></v-divider>
+
                 <span class="small-caps ml-4">Price History</span>
 
                 <v-row class="ml-4">
@@ -115,7 +127,7 @@
                 <v-row v-if="states.bitcoin || states.ethereum" justify="space-around">
                     <v-card width="600" theme="dark" class="mt-8">
                         <v-card-title class="small-caps ma-4">
-                            Currency courses (in Eur)
+                            Currency courses
                         </v-card-title>
 
                         <v-divider></v-divider>
@@ -142,18 +154,42 @@
                             <thead>
                                 <tr class="small-caps">
                                     <th class="text-left">Today</th>
-                                    <th v-if="states.week" class="text-left">Last week</th>
-                                    <th v-if="states.month" class="text-left">Last month</th>
-                                    <th v-if="states.any" class="text-left">At {{ getPastDate() }}</th>
+                                    <th v-if="states.week" class="text-left">Change (week)</th>
+                                    <th v-if="states.month" class="text-left">Change (month)</th>
+                                    <th v-if="states.anytime" class="text-left">Change ({{ getPastDate(getDayDifference()) }})</th>
                                 </tr>
                             </thead>
 
                             <tbody>
                                 <tr>
-                                    <td>{{ prices.bitcoin }}</td>
-                                    <td v-if="states.week">{{ prices.bitcoin.week }}</td>
-                                    <td v-if="states.month">{{ prices.bitcoin.month }}</td>
-                                    <td v-if="states.any">Upcoming</td>
+                                    <td>{{ prices.bitcoin.current }} €</td>
+
+                                    <td v-if="states.week">
+                                        {{ getPercentage(prices.bitcoin.current, prices.bitcoin.week) }} %
+                                        <v-chip label>
+                                            <template v-slot:append>
+                                                <img alt="" :src="iconPaths.up" width="32" height="32" />
+                                            </template>
+                                        </v-chip>
+                                    </td>
+
+                                    <td v-if="states.month">
+                                        {{ getPercentage(prices.bitcoin.current, prices.bitcoin.month) }} %
+                                        <v-chip label>
+                                            <template v-slot:append>
+                                                <img alt="" :src="iconPaths.up" width="32" height="32" />
+                                            </template>
+                                        </v-chip>
+                                    </td>
+
+                                    <td v-if="states.anytime">
+                                        {{ getPercentage(prices.bitcoin.current, prices.bitcoin.anytime) }} %
+                                        <v-chip label>
+                                            <template v-slot:append>
+                                                <img alt="" :src="iconPaths.up" width="32" height="32" />
+                                            </template>
+                                        </v-chip>
+                                    </td>
                                 </tr>
                             </tbody>
                         </v-table>
@@ -166,7 +202,7 @@
                             variant="flat"
                             >
                             <template v-slot:prepend>
-                                <img alt="Ethereum" src="../assets/icons/IconEthereum.svg" width="40" height="45" />
+                                <img alt="Ethereum" src="../assets/icons/IconEthereum.svg" width="40" height="40" />
                             </template>
                             Ethereum (ETH)
                         </v-btn>
@@ -179,18 +215,42 @@
                             <thead>
                                 <tr class="small-caps">
                                     <th class="text-left">Today</th>
-                                    <th v-if="states.week" class="text-left">Last week</th>
-                                    <th v-if="states.month" class="text-left">Last month</th>
-                                    <th v-if="states.any" class="text-left">At {{ getPastDate() }}</th>
+                                    <th v-if="states.week" class="text-left">Change (week)</th>
+                                    <th v-if="states.month" class="text-left">Change (month)</th>
+                                    <th v-if="states.anytime" class="text-left">Change ({{ getPastDate(getDayDifference()) }})</th>
                                 </tr>
                             </thead>
 
                             <tbody>
                                 <tr>
-                                    <td>{{ prices.ethereum }}</td>
-                                    <td v-if="states.week">{{ prices.ethereum.week }}</td>
-                                    <td v-if="states.month">{{ prices.ethereum.month }}</td>
-                                    <td v-if="states.any">Upcoming</td>
+                                    <td>{{ prices.ethereum.current }} €</td>
+
+                                    <td v-if="states.week">
+                                        {{ percentage = getPercentage(prices.ethereum.current, prices.ethereum.week) }} %
+                                        <v-chip label>
+                                            <template v-slot:append>
+                                                <img alt="" :src="iconPaths.up" width="32" height="32" />
+                                            </template>
+                                        </v-chip>
+                                    </td>
+
+                                    <td v-if="states.month">
+                                        {{ getPercentage(prices.ethereum.current, prices.ethereum.month) }} %
+                                        <v-chip label>
+                                            <template v-slot:append>
+                                                <img alt="" :src="iconPaths.up" width="32" height="32" />
+                                            </template>
+                                        </v-chip>
+                                    </td>
+
+                                    <td v-if="states.anytime">
+                                        {{ getPercentage(prices.ethereum.current, prices.ethereum.anytime) }} %
+                                        <v-chip label>
+                                            <template v-slot:append>
+                                                <img alt="" :src="iconPaths.up" width="32" height="32" />
+                                            </template>
+                                        </v-chip>
+                                    </td>
                                 </tr>
                             </tbody>
                         </v-table>
@@ -215,7 +275,6 @@ export default {
     },
     data() {
         return {
-            date: '',
             endpoints: {
                 bitcoin: {
                     price: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur',
@@ -226,9 +285,26 @@ export default {
                     history: 'https://api.coingecko.com/api/v3/coins/ethereum/history?',
                 }
             },
-            prices: {},
+            iconPaths: {
+                up: '/src/assets/icons/IconUpArrow.svg',
+                down: '/src/assets/icons/IconDownArrow.svg'
+            },
+            prices: {
+                bitcoin: {
+                    current: null,
+                    week: null,
+                    month: null,
+                    anytime: null
+                },
+                ethereum: {
+                    current: null,
+                    week: null,
+                    month: null,
+                    anytime: null
+                },
+            },
             states: {
-                any: false,
+                anytime: false,
                 bitcoin: true,
                 chart: false,
                 ethereum: true,
@@ -240,7 +316,25 @@ export default {
     },
     methods: {
         /*
-            get the (past) date and reformat it to match coinGeckos API specs (format: 'dd-mm-yyyy')
+            compose API URL to fetch data from
+
+            @param id <String>
+            @param period <Integer>
+
+            @return <String>
+        */
+        getApiUrl(id = 'bitcoin', period = 0) {
+            const date = this.getPastDate(period)
+
+            return period === 0
+                ? this.endpoints[id].price
+                // it's necessary to replace the date portion here, otherwise the date gets appended everytime the function is called
+                : this.endpoints[id].history = `${this.endpoints[id].history.replace(/date=.*$/, '')}date=${date}`
+        },
+        /*
+            get the date and reformat it to match coinGecko's API specs (format: 'dd-mm-yyyy')
+
+            @param period <Integer>
 
             @return <String>
         */
@@ -249,20 +343,33 @@ export default {
             // change date to [period] days in the past
             if (period !== 0) currentDate.setDate(currentDate.getDate() - period)
 
-           return `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`
+            return `${currentDate.getDate()}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`
         },
         /*
-            define URL to get data from and fetch it from coinGecko
+            calculate the time difference of two dates in milliseconds
 
+            @return <Integer>
+        */
+        getDayDifference() {
+            const date = document.getElementById('datetime').value
+                ? document.getElementById('datetime').value
+                : document.getElementById('datetime').defaultValue
+
+            const currentDate = new Date()
+            const pastDate = new Date(date)
+
+            const differenceTime = currentDate.getTime() - pastDate.getTime()
+            // calculate the number of days ==> ms * sec * min * h
+            return Math.round(differenceTime / (1000 * 60 * 60 * 24) - 1)
+        },
+        /*
+            fetch data from coinGecko
+
+            @param id <String> :: denotes the currency; e.g. bitcoin
             @param period <Integer>
-            @param id <String>
-            */
-        getPrices(period = 0, id = 'bitcoin') {
-            const date = this.getPastDate(period)
-
-            const apiUrl = period === 0
-                ? this.endpoints[id].price
-                : this.endpoints[id].history = `${this.endpoints[id].history}date=${date}`
+        */
+        getPrices(id = 'bitcoin', period = 0) {
+            const apiUrl = this.getApiUrl(id, period)
 
             fetch(apiUrl)
                 .then((response) => {
@@ -271,34 +378,63 @@ export default {
                     }
                     return response.json()
                 })
-                .then((data) => {
-                    if (period === 0) {
-                        const { bitcoin, ethereum } = data
-                        console.log(data)
+                .then((data ) => {
+                    const currency = id === 'bitcoin'
+                        ? 'bitcoin'
+                        : 'ethereum'
 
-                        this.prices[id]['current'] = id === 'bitcoin'
-                            ? bitcoin
-                            : ethereum
-                    } else {
+                    if (period === 0) {
+                        if (id === 'bitcoin') {
+                            const { bitcoin } = data
+                            const { eur } = bitcoin
+
+                            this.prices[currency].current = Math.round(eur)
+                        }
+                        else {
+                            const { ethereum } = data
+                            const { eur } = ethereum
+
+                            this.prices[currency].current = Math.round(eur)
+                        }
+                    }
+                    else {
                         const { market_data } = data
                         const { current_price } = market_data
                         const { eur } = current_price
-    
-                        period === 7
-                            ? this.prices[id]['week'] = eur
-                            : this.prices[id]['month'] = eur
+
+                        switch (period) {
+                            case 7:
+                                this.prices[currency].week = Math.round(eur)
+                            break
+                            case 30:
+                                this.prices[currency].month = Math.round(eur)
+                            break
+                            default:
+                                this.prices[currency].anytime = Math.round(eur)
+                        }
                     }
                 })
                 .catch((err) => console.error(`Server error: ${err.message}`))
         },
+        /*
+            calculate the ratio between the current priceand the past one
+
+            @param currentprice <Float>
+            @param pastPrice <Float>
+
+            @return <String>
+        */
+        getPercentage(currentPrice, pastPrice) {
+            // the prepended '+' gets rid of trailing zeros; e.g. 1.50 ==> 1.5
+            return +((currentPrice / pastPrice * 100) - 100).toFixed(2)
+        },
         init() {
-            this.getPrices(0, 'bitcoin')
-            this.getPrices(0, 'ethereum')
-        }
-    },
-    computed: {
-        weekAgo() {
-            return Math.round((this.prices.bitcoin.week / 100))
+            const periods = [0, 7, 30]
+
+            for (let period of periods) {
+                this.getPrices('bitcoin', period)
+                this.getPrices('ethereum', period)
+            }
         }
     },
     mounted() {
