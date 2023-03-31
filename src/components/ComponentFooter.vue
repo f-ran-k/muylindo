45561f6b371b41aca353dbbe517f4f681d088162
Frank <frank.schneider@tutanota.com>
2023-03-31 10:41:10 +0200

    add github svg, github repo link to the footer, aand titles to the footer icons

--- src/components/ComponentFooter.vue
+++ src/components/ComponentFooter.vue
@@ -1,17 +1,23 @@
 <template>
     <v-footer app class="small-caps">Build w/
         <a href="https://vuejs.org" target="_blank" class="mx-4">
-            <img alt="Vue.js" src="../assets/icons/IconVue.svg" width="40" height="40" />
+            <img title="Vue.js" alt="Vue.js" src="../assets/icons/IconVue.svg" width="40" height="40" />
         </a>
             &nbsp;+&nbsp;
         <a href="https://vuetifyjs.com" target="_blank" class="mx-4">
-            <img alt="Vuetify" src="../assets/icons/IconVuetify.svg" width="40" height="40" />
+            <img title="Vuetify" alt="Vuetify" src="../assets/icons/IconVuetify.svg" width="40" height="40" />
         </a>
 
-        <span>CoinGecko API</span>
+        <span class="ml-16">CoinGecko API</span>
 
-        <a href="https://www.coingecko.com/en/api/documentation" target="_blank" class="ml-8">
-            <img alt="CoinGecko" src="../assets/icons/IconGecko.svg" width="40" height="40" />
-        </a>        
+        <a href="https://www.coingecko.com/en/api/documentation" target="_blank" class="mx-4">
+            <img title="CoinGecko" alt="CoinGecko" src="../assets/icons/IconGecko.svg" width="40" height="40" />
+        </a>
+
+        <span class="ml-8">GitHub</span>
+
+        <a href="https://github.com/f-ran-k/muylindo" target="_blank" class="mx-4">
+            <img title="GitHub" alt="GitHub" src="../assets/icons/IconGitHub.svg" width="40" height="40" />
+        </a>
     </v-footer>
 </template>
