beb37bf19410bc0cedc0167940bb3e8b9596f42e
Frank <frank.schneider@tutanota.com>
2023-04-04 00:01:27 +0200

    style: add grid to the footer

--- src/components/ComponentFooter.vue
+++ src/components/ComponentFooter.vue
@@ -1,23 +1,37 @@
 <template>
-    <v-footer app class="small-caps">Build w/
-        <a href="https://vuejs.org" target="_blank" class="mx-4">
-            <img title="Vue.js" alt="Vue.js" src="@/assets/icons/IconVue.svg" width="40" height="40" />
-        </a>
-            &nbsp;+&nbsp;
-        <a href="https://vuetifyjs.com" target="_blank" class="mx-4">
-            <img title="Vuetify" alt="Vuetify" src="@/assets/icons/IconVuetify.svg" width="40" height="40" />
-        </a>
+    <v-footer app>
+        <v-container fill-height>
+            <v-row justify="center" class="small-caps">
+                <v-col>
+                    <span>Build w/</span>
 
-        <span class="ml-16">CoinGecko API</span>
+                    <a href="https://vuejs.org" target="_blank" class="mx-4">
+                        <img title="Vue.js" alt="Vue.js" src="@/assets/icons/IconVue.svg" width="40" height="40" />
+                    </a>
 
-        <a href="https://www.coingecko.com/en/api/documentation" target="_blank" class="mx-4">
-            <img title="CoinGecko API Playground" alt="CoinGecko" src="@/assets/icons/IconGecko.svg" width="40" height="40" />
-        </a>
+                    <span>&nbsp;+&nbsp;</span>
 
-        <span class="ml-8">GitHub</span>
+                    <a href="https://vuetifyjs.com" target="_blank" class="mx-4">
+                        <img title="Vuetify" alt="Vuetify" src="@/assets/icons/IconVuetify.svg" width="40" height="40" />
+                    </a>
+                </v-col>
 
-        <a href="https://github.com/f-ran-k/muylindo" target="_blank" class="mx-4">
-            <img title="GitHub Repo" alt="GitHub" src="@/assets/icons/IconGitHub.svg" width="40" height="40" />
-        </a>
+                <v-spacer></v-spacer>
+
+                <v-col>
+                    <span>CoinGecko</span>
+
+                    <a href="https://www.coingecko.com/en/api/documentation" target="_blank" class="mx-4">
+                        <img title="CoinGecko API Playground" alt="CoinGecko" src="@/assets/icons/IconGecko.svg" width="40" height="40" />
+                    </a>
+
+                    <span>GitHub</span>
+
+                    <a href="https://github.com/f-ran-k/muylindo" target="_blank" class="mx-4">
+                        <img title="GitHub Repo" alt="GitHub" src="@/assets/icons/IconGitHub.svg" width="40" height="40" />
+                    </a>
+                </v-col>
+            </v-row>
+        </v-container>
     </v-footer>
 </template>
