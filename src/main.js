333f77d57becbf6f3d0b81397838c4ad15030954
Frank <frank.schneider@tutanota.com>
2023-04-12 15:40:41 +0200

    feat: add eventBus via mitt to mimiv Vue v2.x's global event handling

--- src/main.js
+++ src/main.js
@@ -8,7 +8,17 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+/**
+ * mimic Vue v2.x's behaviour of providing an EventBus
+ * 
+ * call this.$eventBus.emit() instead of this.$root.emit() to make an event globally available
+ * or rather this.$eventBus.on() instead of this.$root.on() to listen for custom gloabl events respectively
+*/
+import mitt from 'mitt'
+
 const app = createApp(App)
+
+app.config.globalProperties.$eventBus = mitt()
 
 const vuetify = createVuetify({
   components,
