44f138fcbaaa4e78c77d8150bee606d5375bbd37
Frank <frank.schneider@tutanota.com>
2023-04-12 16:04:05 +0200

    refactor: add computed values to check for element disabling instead of checking in the template

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,5 +1,5 @@
-<template v-if="States && history">
-    <v-navigation-drawer class="small-caps" height="auto" width="400">
+<template>
+    <v-navigation-drawer class="small-caps" width="400">
         <v-list>
             <v-list-item class="bg-grey text-h6">Controls</v-list-item>
         </v-list>
@@ -29,7 +29,7 @@
         </v-row>
 <!-- Currency end -->
         <v-divider class="mb-4"></v-divider>
-<!-- History && Chart -->
+<!-- Courses, History, Chart -->
         <span class="ml-4">Data to display</span>
 
         <v-row class="mx-4 mt-4">
@@ -44,23 +44,23 @@
                     v-model="States[dataType]"
                     color="blue-grey"
                     :label="States[dataType] ? 'Hide' : 'Show'"
-                    :disabled="!(States.bitcoin || States.ethereum)"
+                    :disabled="elementDisabled"
                 >
                 </v-switch>
             </v-col>
         </v-row>
 
-        <v-row v-if="(States.history || States.chart) && !(history.bitcoin || history.ethereum)" class="ma-4 mt-0">
+        <v-row v-if="noData" class="ma-4 mt-0">
             <v-alert
                 border="start"
                 border-color="warning"
                 elevation="2"
                 title="No Data fetched yet!"
             >
-                <span style="font-weight: normal;">Please select a date from the date picker below to display the price history and chart.</span>
+                <span class="font-weight-normal">Please select a date from the date picker below to display the price history and chart.</span>
             </v-alert>
         </v-row>
-<!-- History && Chart end -->
+<!-- Courses, History, Chart end -->
         <v-divider class="mb-4"></v-divider>
 <!-- Period -->
         <span class="ml-4">Select a period</span>
@@ -71,11 +71,13 @@
                     v-model="States[period]"
                     color="cyan"
                     :label="`Last ${period}`"   
-                    :disabled="!States.courses || !(States.bitcoin || States.ethereum)"
+                    :disabled="!States.courses || elementDisabled"
                     >
                 </v-switch>
             </v-col>
         </v-row>
+<!-- Period end -->
+
 <!-- Date Picker -->
         <span class="ml-4">Select a date</span>
 
@@ -85,23 +87,23 @@
                     v-model="States.anytime"
                     color="cyan"
                     :label="States.anytime ? 'Hide' : 'Show'"
-                    :disabled="!(States.bitcoin || States.ethereum)"
+                    :disabled="elementDisabled"
                 >
                 </v-switch>
             </v-col>
 
             <v-col cols="8" v-if="States.anytime">
                 <v-row class="mt-4 ml-4">
-                    <v-form @submit.prevent>
-                        <input id="datetime" type="date" value="2023-04-01" :disabled="!(States.bitcoin || States.ethereum)" />
+                    <v-form @submit.prevent :disabled="elementDisabled">
+                        <input id="datetime" type="date" value="2023-04-01" :disabled="elementDisabled" />
 
                         <v-btn
                             class=" ml-2"
                             color="grey"
                             size="small"
                             title="Send"
-                            :disabled="!(States.bitcoin || States.ethereum)"
                             @click="$emit('update-price')"
+                            :disabled="elementDisabled"
                         >
                             <span>Send</span>
                         </v-btn>
@@ -109,7 +111,7 @@
                 </v-row>
             </v-col>
         </v-row>
-<!-- Date Picker && Period end -->
+<!-- Date Picker end -->
         <v-divider class="mb-4"></v-divider>
     </v-navigation-drawer>
 </template>
@@ -132,6 +134,14 @@ export default {
         return {
             States: this.states,
         }
+    },
+    computed: {
+        elementDisabled() {
+            return !(this.States.bitcoin || this.States.ethereum)
+        },
+        noData() {
+            return (this.States.history || this.States.chart) && !(Object.keys(this.history.bitcoin).length || Object.keys(this.history.ethereum).length)
+        },
     },
 }
 </script>
